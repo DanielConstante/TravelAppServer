e68e10d306bb0514cb98d1867a30a2acdb5740c5
Daniel Constante <daniel_constante86@hotmail.com>
2021-09-11 12:50:01 -0500

    Week 3 Workshop Assignment

---  authenticate.js
+++  authenticate.js
@@ -38,3 +38,13 @@ exports.getToken = function(user) {
   );
   
   exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+  exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin){
+        return next();
+    } else {
+        const err = new Error('You are not authorized to perform this operation!');
+        err.status = 403 ;
+        return next(err);
+    }
+};
